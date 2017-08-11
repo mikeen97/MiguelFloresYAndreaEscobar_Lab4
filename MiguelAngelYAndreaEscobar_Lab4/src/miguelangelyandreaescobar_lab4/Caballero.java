a9d0d727d63fbc0a0a2ec61310340eed6d82d197
mikeen97 <mike97@hotmail.es>
2017-08-11 17:40:11 -0600

    movimientos

--- MiguelAngelYAndreaEscobar_Lab4/src/miguelangelyandreaescobar_lab4/Caballero.java
+++ MiguelAngelYAndreaEscobar_Lab4/src/miguelangelyandreaescobar_lab4/Caballero.java
@@ -12,8 +12,62 @@ public class Caballero extends Pieza {
     }
 
     @Override
-    public int movimiento(Pieza[][] matriz, int posx, int posy, int moverx, int movery, Color color) {
-        return 1;
+    public int movimiento(Pieza[][] matriz, int posx, int posy, int moverx, int movery, Color color) throws Excepciones {
+        int movimiento = 0, contx = 0, fin = 0, conty = 0;
+        int pasos = posx - moverx;
+        conty = posy - movery;
+        Pieza e = new EspacioBlanco();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (posx == moverx && fin == 0) {
+                    if (i == posx && j == posy) {
+                        while (contx < pasos && fin == 0) {
+                            if (matriz[posx + contx][posy] == e) {
+                                movimiento = 1;//Avanza;
+                                fin = 1;
+                            }
+                            contx++;
+                        }
+                        while (contx < pasos && fin == 0) {
+                            if (matriz[posx + contx][posy] != e) {
+                                movimiento = 2;//No Avanza;
+                                fin = 1;
+                            }
+                            contx++;
+                        }
+                    }
+                    if (fin == 0) {
+                        movimiento = 3;//No Avanza;
+                        fin = 1;
+                    }
+                }
+                if (posy == movery && fin == 0) {
+                    if (i == posx && j == posy) {
+                        while (conty < pasos && fin == 0) {
+                            if (matriz[posx][posy + conty] == e) {
+                                movimiento = 1;//Avanza;
+                                fin = 1;
+                            }
+                            conty++;
+                        }
+                        while (conty < pasos && fin == 0) {
+                            if (matriz[posx][posy + conty] != e) {
+                                movimiento = 2;//No Avanza;
+                                fin = 1;
+                            }
+                            conty++;
+                        }
+                    }
+                    if (fin == 0) {
+                        movimiento = 3;//No Avanza;
+                        fin = 1;
+                    }
+                }
+
+            }
+        }
+
+        return movimiento;
     }
 
     @Override
