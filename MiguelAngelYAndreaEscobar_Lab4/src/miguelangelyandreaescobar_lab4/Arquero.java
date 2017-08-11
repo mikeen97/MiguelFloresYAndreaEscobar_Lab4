b8184cdb5ced8080120a8015335a6bbdf03846d6
Andrea <andreajaninaescobar@gmail.com>
2017-08-11 17:39:22 -0600

    arquero

--- MiguelAngelYAndreaEscobar_Lab4/src/miguelangelyandreaescobar_lab4/Arquero.java
+++ MiguelAngelYAndreaEscobar_Lab4/src/miguelangelyandreaescobar_lab4/Arquero.java
@@ -17,8 +17,31 @@ public class Arquero extends Pieza {
     }
 
     @Override
-    public int movimiento(Pieza[][] matriz, int posx, int posy, int moverx, int movery, Color color) throws Excepciones{
-        
+    public int movimiento(Pieza[][] matriz, int posx, int posy, int moverx, int movery, Color color) throws Excepciones {
+        int move = 0;
+        Pieza e = new EspacioBlanco();
+        if ((moverx > 9 || movery > 9) || (moverx < 0 || movery < 0)) {
+            throw new Excepciones("Fuera de tablero");
+        } else {
+            if (matriz[posx][posy].getColor() == Color.BLACK) {
+                if (matriz[moverx][movery]== e) {
+                    if (true) {
+                        
+                    }
+                }
+                else{
+                    if (matriz[moverx][movery]!=e) {
+                        if (matriz[moverx][movery].getColor() == Color.BLACK) {
+                            move =2;
+                        }
+                        else{
+                            move = 3;
+                        }
+                    }
+                }
+            }
+        }
+        return move;
     }
 
 }
--- MiguelAngelYAndreaEscobar_Lab4/src/miguelangelyandreaescobar_lab4/Dragon.java
+++ MiguelAngelYAndreaEscobar_Lab4/src/miguelangelyandreaescobar_lab4/Dragon.java
@@ -2,7 +2,6 @@ package miguelangelyandreaescobar_lab4;
 
 import java.awt.Color;
 
-
 public class Dragon extends Pieza {
 
     public Dragon() {
@@ -18,8 +17,29 @@ public class Dragon extends Pieza {
     }
 
     @Override
-        public int movimiento(Pieza[][] matriz, int posx, int posy, int moverx, int movery, Color color)throws Excepciones {
-            return 1;
+    public int movimiento(Pieza[][] matriz, int posx, int posy, int moverx, int movery, Color color) throws Excepciones {
+        Pieza e = new EspacioBlanco();
+        int mx = posx - moverx, my = posy - movery;
+        int mx1 = (int) Math.abs(mx);
+        int my1 = (int) Math.abs(my);
+        int cont = 0, pasos = mx1;
+        if ((moverx > 9 || movery > 9) || (moverx < 0 || movery < 0)) {
+            throw new Excepciones("Fuera de tablero");
+        }
+        if (matriz[posx][posy].getColor() == (Color.BLACK)) {
+            if (mx1 == my1) {
+                if (mx < 0) {//IZquierda
+                    while (cont < mx1) {
+                        
+                    }
+                }
+                if (mx > 0) {//Derecha
+                    while (cont < mx1) {
+
+                    }
+                }
+            }
+        }
     }
-    
+
 }
