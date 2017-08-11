f2ca4253d6dd2d95907f8d329713fb1255596aaa
Andrea <andreajaninaescobar@gmail.com>
2017-08-11 15:34:52 -0600

    ya

--- MiguelAngelYAndreaEscobar_Lab4/src/miguelangelyandreaescobar_lab4/Dragon.java
+++ MiguelAngelYAndreaEscobar_Lab4/src/miguelangelyandreaescobar_lab4/Dragon.java
@@ -13,6 +13,11 @@ public class Dragon extends Pieza {
     }
 
     @Override
+    public String toString() {
+        return "F";
+    }
+
+    @Override
         public boolean movimiento(Pieza[][] matriz, int posx, int posy, int moverx, int movery, Color color) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
